1b1e273e76224e3b88078803572cdef3e965d963
アリス <161630589+Nian0bussou@users.noreply.github.com>
2024-10-21 11:39:40 -0400

    sstad

--- TestAppProject/Program.cs
+++ TestAppProject/Program.cs
@@ -1,6 +1,20 @@
 ﻿using GraphLib;
 
-var a = new AdjacencyMatrix(5);
+//  partie 1
 
-a[0, 0] = 1;
-a.AddEdge(0, 0, 1);
+{
+    var a = new AdjacencyMatrix(6);
+
+    a[0, 1] = 1;
+    a[0, 2] = 3;
+
+    a[1, 0] = 1;
+    a[1, 3] = 4;
+    a[1, 4] = 10;
+    a[2, 3] = 3;
+
+    a[3, 4] = 1;
+    a[3, 6] = 1;
+
+    Console.WriteLine(a);
+}
